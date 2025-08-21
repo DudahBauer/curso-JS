7c20a0f23d9d86eb543b4a90519c0fddcb45c867
Maria Eduarda <mevbauer@gmail.com>
2025-08-21 19:52:54 -0300

    adicionando mudanças

--- exercicios/exercicios-de-fixacao/calculadora-site/js/script.js
+++ exercicios/exercicios-de-fixacao/calculadora-site/js/script.js
@@ -1,59 +1,29 @@
-
-let primeiroNumero = document.getElementById("nOne")
-let segundoNumero = document.getElementById("nTwo")
-let soma =document.getElementById("adicao")
-let menos = document.getElementById("subtracao")
-let vezes = document.getElementById("multiplicacao")
-let dividir = document.getElementById("divisao")
-let enviarButton = document.getElementById("enviar")
 let frase = document.getElementById("result")
+let form = document.getElementById("my-form")
 
-enviarButton.addEventListener("click", primeiroNumero, segundoNumero, soma, menos, vezes, dividir)
-var numero1= parseFloat(primeiroNumero.value);
-var numero2 = parseFloat(segundoNumero.value);
+form.addEventListener("submit", envioDoFormulario)
 
-function numero (event){
+function envioDoFormulario(event){
     event.preventDefault();
+    const formData = new FormData(event.target)
+    let primeiroNumero = Number(formData.get("numberOne"))
+    let segundoNumero = Number(formData.get("numberTwo"))
+    let operacao = formData.get("operacao")
+    let resposta=0
 
-    var resultado= getOperacao()
-    frase.innerText= `O resultado é: ${resultado}`
+    if(operacao=="+"){
+       resposta= primeiroNumero+segundoNumero
+    }
+    else if(operacao=="-"){
+      resposta=  primeiroNumero-segundoNumero
 
     }
-
-
-function getOperacao(){
-    if(soma.checked){
-        numero1+numero2
+    else if(operacao=="*"){
+       resposta= primeiroNumero*segundoNumero
     }
+    else if(operacao=="/")
+    resposta= primeiroNumero/segundoNumero
 
-    if(menos.checked){
-        numero1-numero2
-
-    }
-
-    if(vezes.checked){
-        numero1*numero2
-    }
-
-    if(dividir.checked){
-        numero1/numero2
-    }
+    frase.innerText= `O resultado é: ${resposta}`
 }
 
-
-
-// form.addEventListener('submit', function(event){
-
-//     event.preventDefault();
-//     const numero1= parseFloat(primeiroNumero.value);
-//     const numero2= parseFloat(segundoNumero.value);
-
-//     const operacaoSelecionada = document.querySelector('input[name="operacao"]:checked');
-//     const operacao = operacaoSelecionada.value;
-
-//     switch(operacao){
-//         case 'adicao':
-//             console.log("resultado:", numero1+numero2)
-
-//     }
-// });
